31a0a762fd2cbf512013ec3ff5c7f7bfd9856311
Kavitha U <kavithavuppin@gmail.com>
2025-01-23 16:34:59 -0600

    Added jar

--- src/main/java/com/Tekarch/TafDatastoreService/Controllers/BookingsController.java
+++ src/main/java/com/Tekarch/TafDatastoreService/Controllers/BookingsController.java
@@ -12,7 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/bookings")
@@ -31,10 +35,8 @@ public class BookingsController {
         if (booking.getUser() == null || booking.getUser().getId() == null) {
             return ResponseEntity.badRequest().body(null); // User not provided
         }
-        Flights flight = flightRepository.findById(booking.getFlight().getId())
-                .orElseThrow(() -> new RuntimeException("Flight not found with id " + booking.getFlight().getId()));
-        Users user = userRepository.findById(booking.getUser().getId())
-                .orElseThrow(() -> new RuntimeException("User not found with id " + booking.getUser().getId()));
+        Flights flight = flightRepository.findById(booking.getFlight().getId()).orElseThrow(() -> new RuntimeException("Flight not found with id " + booking.getFlight().getId()));
+        Users user = userRepository.findById(booking.getUser().getId()).orElseThrow(() -> new RuntimeException("User not found with id " + booking.getUser().getId()));
         booking.setFlight(flight);
         booking.setUser(user);
         booking.setStatus("Booked");
@@ -44,8 +46,10 @@ public class BookingsController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Bookings> updateBooking(@PathVariable Long id, @RequestBody Bookings bookingDetails) {
-        Bookings booking = bookingRepository.findById(id).orElseThrow(() -> new RuntimeException("Booking not found with id " + id));
-        Flights flight = flightRepository.findById(bookingDetails.getFlight().getId()).orElseThrow(() -> new RuntimeException("Flight not found with id " + bookingDetails.getFlight().getId()));
+        Bookings booking = bookingRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Booking not found with id " + id));
+        Flights flight = flightRepository.findById(bookingDetails.getFlight().getId())
+                .orElseThrow(() -> new RuntimeException("Flight not found with id " + bookingDetails.getFlight().getId()));
 
         booking.setUser(booking.getUser());
         booking.setFlight(flight);
@@ -55,18 +59,23 @@ public class BookingsController {
         return ResponseEntity.ok(updatedBooking);
     }
 
+
     @GetMapping("/{id}")
     public ResponseEntity<Bookings> getBookingById(@PathVariable Long id) {
-        return bookingRepository.findById(id)
-                .map(ResponseEntity::ok)
+        return bookingRepository.findById(id).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<List<Bookings>> getBookingByUserId(@PathVariable Long id) {
+        List<Bookings> bkList = bookingRepository.findByUserId(id);
+        return bkList.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(bkList);
     }
 
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBooking(@PathVariable Long id) {
-        Bookings booking = bookingRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Booking not found with id " + id));
+        Bookings booking = bookingRepository.findById(id).orElseThrow(() -> new RuntimeException("Booking not found with id " + id));
         booking.setStatus("Cancelled");
         bookingRepository.save(booking);
         return ResponseEntity.noContent().build();
